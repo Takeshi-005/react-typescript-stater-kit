34b027b138e557235dbe11b1f06094469677071f
tkctkc <38493534+Takeshi-005@users.noreply.github.com>
2020-03-25 18:30:08 +0900

    Update .eslintrc.js
    eslintrcを更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,64 +1,63 @@
+const path = require("path");
+
 module.exports = {
   env: {
     browser: true,
     es6: true,
     node: true,
-    'jest/globals': true
   },
   extends: [
-    'airbnb',
-    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended",
     'plugin:import/errors',
     'plugin:import/warnings',
-    'plugin:prettier/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react'
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "prettier/react"
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
     '__DEV__': true
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
     sourceType: 'module'
   },
   plugins: [
-    '@typescript-eslint',
-    'jest',
-    'prettier',
-    'prefer-arrow',
+    "@typescript-eslint",
     'react',
+    'prefer-arrow',
     'react-hooks'
   ],
-  root: true,
+  root: true, //このプロジェクト内のみに適用 (デフォルトは親ディレクトリまで遡って適用する)
   settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', 'jsx', '.ts', '.tsx']
-      }
-    },
+    // 'import/resolver': {
+      // eslint-import-resolver-webpackを使用する
+      // webpack: {
+      //   config: path.join(__dirname, './webpack.web.config.js')
+      //   }
+      // node: {
+      //   extensions: ['.js', 'jsx', '.ts', '.tsx']
+      // }
+    // },
     react: {
       version: 'detect'
     }
   },
   rules: {
     // eslint official
-    'newline-before-return': 'error',
     'no-console': 'warn',
-    'require-yield': 'error',
+    "no-unused-vars": "error",
 
     // @typescript-eslint
+    "@typescript-eslint/camelcase": "off",
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
-    indent: 'off',
-    '@typescript-eslint/indent': 'off',
-    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-parameter-properties': 'off',
 
     // prefer-arrow
     'prefer-arrow/prefer-arrow-functions': [
@@ -85,6 +84,7 @@ module.exports = {
 
     // react hooks
     'react-hooks/rules-of-hooks': 'error',
+    "react-hooks/exhaustive-deps": 'warn',
 
     // import
     'import/extensions': [
@@ -99,16 +99,14 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
 
-    // prettier
-    'prettier/prettier': [
-      'error', {
+    "prettier/prettier": [
+      "error",
+      {
         bracketSpacing: true,
         printWidth: 80,
-        semi: true,
-        singleQuote: true,
-        trailingComma: 'all',
+        tabWidth: 2,
         useTabs: false
       }
     ]
   }
-};
+}
