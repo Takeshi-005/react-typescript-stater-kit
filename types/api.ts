aff6d839bbe2beb8761a9ea9f579072f5067e479
tkctkc <38493534+Takeshi-005@users.noreply.github.com>
2020-03-27 14:50:38 +0900

    Update api.ts
    型を更新

--- types/api.ts
+++ types/api.ts
@@ -1,6 +1,18 @@
+import { AxiosError } from 'axios';
+export interface ApiConfig {
+  baseURL: string;
+  timeout: number;
+}
+
 export type Products = {
   id: number;
   title: string;
   description: string;
   availableSizes: string[];
 };
+
+export type ApiState = {
+  products: Products[];
+  isLoading: boolean;
+  error?: AxiosError | null;
+}
