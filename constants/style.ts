9e00a36ab222db568eb0db82c5bf661fc2171c17
tkctkc <38493534+Takeshi-005@users.noreply.github.com>
2020-03-30 17:40:37 +0900

    Create style.ts
    constatsのstyleファイルを追加

---
+++ constants/style.ts
@@ -0,0 +1,12 @@
+// styleで使用する定数を管理する
+export const BUTTON = {
+  primary: {
+    color: "rgb(255, 255, 255)",
+    bg: "rgb(234, 53, 45)",
+    border: "rgb(234, 53, 45)"
+  },
+  secondary: {
+    color: "rgb(0,0,0)",
+    bg: "#ff0"
+  }
+} as const
